305173d67eee47fd8dd248e24709b04a65a86d77
andrea matte <30327952+andreamatt@users.noreply.github.com>
2018-01-05 23:45:04 +0100

    Now able to create v1 VPKs
    - added VPKcreator class: use load and write to create a vpk file of a directory (ONLY FOR V1 VPK)
    - now exports { VPK, VPKcreator }

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ let TYPESET = {
                 crc: 'uint32',				// crc integrity
 				preloadBytes: 'uint16',		// size of preload (almost always 0) (used for small but critical files)
 				archiveIndex: 'uint16',		// on which archive the data is stored (7fff means on _dir archive)
-				entryOffset: 'uint32',		// if on _dir, this is offset of data from dirEntry end. If on other archive, offset from start of it
+				entryOffset: 'uint32',		// if on _dir, this is offset of data from tree end. If on other archive, offset from start of it
 				entryLength: 'uint32'		// size of data
             });
 
@@ -89,7 +89,7 @@ let TYPESET = {
                             fullPath = directory + '/' + fullPath;
                         }
 
-                        let entry = this.binary.read('vpkDirectoryEntry');
+						let entry = this.binary.read('vpkDirectoryEntry');
                         entry.preloadOffset = this.binary.tell();
 
                         this.binary.skip(entry.preloadBytes);
@@ -247,4 +247,130 @@ class VPK {
 	}
 }
 
-module.exports = VPK;
+function listFiles(directory, first = true, root = directory){
+	var files = [];
+	var	filesHere = fs.readdirSync(directory);
+	filesHere.forEach(file => {
+		if(fs.statSync(directory + "/" + file).isDirectory()){
+			files = files.concat(listFiles(directory + "/" + file, false, root));
+		}
+		else{
+			files.push({
+				location: 	first ? " " : directory.substr(root.length + 1),
+				name: 		file.substr(0, file.lastIndexOf(".")),
+				extension: 	file.substr(file.lastIndexOf(".") + 1),
+				crc: crc.crc32(fs.readFileSync(directory + "/" + file)),
+				dataSize: fs.statSync(directory + "/" + file).size,
+				fullPath: directory + "/" + file
+			});
+		}
+	});
+
+	return files;
+}
+
+class VPKcreator {
+	constructor(directory){
+		this.root = directory;
+		this.loaded = false;
+	}
+
+	isValid(){
+		try {
+			if(fs.statSync(this.root).isDirectory()){
+				return true;
+			}
+			return false;
+		} catch (error) {
+			return false;
+		}
+	}
+
+	// load dir as vpk (only supports VPK 1 atm)
+	load(version = 1){
+		if(version != 1){
+			throw new Error("Version not supported");
+		}
+		if(this.isValid()){
+				
+			// create all entries
+			var files = listFiles(this.root);
+
+			// calculate total size of entries
+			var totalSize = 0;
+			totalSize += 4; // signature
+			totalSize += 4; // vpk version
+			totalSize += 4; // treeSize
+			var treeSize = 0;
+			this.totalData = 0;
+			files.forEach(file => {
+				let entrySize = 0;
+				entrySize = (file.location.length +1) + (file.name.length +1) + (file.extension.length +1);
+				entrySize += 4;	// crc
+				entrySize += 2;	// preloadBytes
+				entrySize += 2; // archiveIndex
+				entrySize += 4; // entryOffset
+				entrySize += 4; // entryLength
+				entrySize += 2; // terminator
+				entrySize += 2; // 2 nulls terminating
+				file.entrySize = entrySize;
+				treeSize += entrySize;
+				this.totalData += file.dataSize;
+			});
+			treeSize += 1; // the last null
+			totalSize += treeSize;
+
+			// set all offsets
+			var offset = 0; // offset from tree end
+			files.forEach(file => {
+				file.dataOffset = offset;
+				offset += file.dataSize;
+			});
+
+			this.totalSize = totalSize;
+			this.treeSize = treeSize;
+			this.tree = files;
+
+			this.loaded = true;
+		}
+	}
+
+	save(destinationFile){
+		// header
+		var header = new Buffer(HEADER_1_LENGTH);
+		header.writeUInt32LE(0x55aa1234, 0);
+		header.writeUInt32LE(1, 4);
+		header.writeUInt32LE(this.treeSize, 8);
+
+		// tree
+		var tree = new Buffer(this.treeSize);
+		var offset = 0;
+		this.tree.forEach(file => {
+			console.log(file);
+			tree.write(file.extension + "\0" + file.location + "\0" + file.name + "\0", offset);
+			let relOff = offset + file.entrySize - 20;
+			tree.writeUInt32LE(file.crc, relOff);				// crc
+			tree.writeUInt16LE(0x0000, relOff +4);				// preloadByes
+			tree.writeUInt16LE(0x7fff, relOff +6);				// archiveIndex
+			tree.writeUInt32LE(file.dataOffset, relOff +8);		// entryOffset
+			tree.writeUInt32LE(file.dataSize, relOff +12);		// entryLength
+			tree.writeUInt16LE(0xffff, relOff +16);				// terminator
+			tree.write("\0\0", relOff +18);						// 2 nulls
+
+			offset += file.entrySize;
+		});
+		tree.write("\0", this.treeSize);			// last null
+
+		fs.writeFileSync(destinationFile, Buffer.concat([header, tree]));
+
+		// data
+		var data = new Buffer(this.totalData);
+		this.tree.forEach(file => {
+			console.log(file);
+			let fileData = fs.readFileSync(file.fullPath);
+			fs.appendFileSync(destinationFile, fileData);
+		});
+	}
+}
+
+module.exports = {VPK, VPKcreator};
